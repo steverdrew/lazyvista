4ed439d9401ef9c2304f863c073ad2745df75604
Steve Drew <steverdrew@gmail.com>
2014-05-12 15:25:43 +0000

    preloader, nav dropdown probably broken

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,33 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.cookie
 //= require foundation
-//= require turbolinks
+
 
 $(function(){ $(document).foundation(); });
+
+$(window).load(function() { // makes sure the whole site is loaded  
+  $("#status").fadeOut(); // will first fade out the loading animation
+	$("#preloader").delay(1000).fadeOut("slow"); // will fade out the white DIV that covers the website.
+})
+
+$(document).ready(function() {
+  
+  menuId = $.cookie("menuId");
+  var e = document.getElementById(menuId)
+  $('.menu-nav-tab').removeClass('active');
+  $(".main-content").children().hide();
+  $(e).addClass("active");
+  $($.cookie("section")).show();
+    
+  $(".menu-nav-tab").click(function() {
+    var parts = this.id.split("-");
+    $(".main-content").children().hide();
+    $('.menu-nav-tab').removeClass('active');
+    $(this).addClass("active");
+    $("#" + parts[0]).show();
+    $.cookie("menuId",this.id);
+    $.cookie("section","#" + parts[0]);
+  });
+});
